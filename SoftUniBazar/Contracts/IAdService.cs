e07ee9f48435f09f255d99d442a14c230a3c11ea
Ivaylo P Kalaydzhiev <160835946+IvoPetkov1985@users.noreply.github.com>
2024-11-04 10:26:54 +0200

    Contracts added.

---
+++ SoftUniBazar/Contracts/IAdService.cs
@@ -0,0 +1,29 @@
+﻿using SoftUniBazar.Models;
+
+namespace SoftUniBazar.Contracts
+{
+    public interface IAdService
+    {
+        Task AddAdToCartAsync(string userId, int id);
+
+        Task<AdFormModel> CreateModelToEditAsync(int id);
+
+        Task CreateNewAdEntityAsync(AdFormModel model, string userId);
+
+        Task DeleteAdAsync(int id);
+
+        Task EditAdAsync(int id, AdFormModel model);
+
+        Task<bool> IsAdExistingAsync(int id);
+
+        Task<IEnumerable<AdAllViewModel>> GetAllAdsAsync();
+
+        Task<IEnumerable<AdCartViewModel>> GetAllAdsInCartAsync(string userId);
+
+        Task<IEnumerable<CategoryViewModel>> GetCategoriesAsync();
+
+        Task RemoveAdFromCartAsync(string userId, int id);
+
+        Task<bool> IsUserAuthorizedAsync(string userId, int id);
+    }
+}
